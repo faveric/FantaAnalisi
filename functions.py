c49bd1b2c2749ddecba3f88723b28b09cc8c4c96
faveric <ricfvt@gmail.com>
2024-08-03 15:01:06 +0200

    requirements.txt added

--- functions.py
+++ functions.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import os
 # import plotly.express as px
-import plotly
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
